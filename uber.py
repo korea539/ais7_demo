e194b1b7fe5571540705869995e589e33c925d70
korea539 <114369511+korea539@users.noreply.github.com>
2022-10-18 17:00:41 +0900

    Update uber.py

--- uber.py
+++ uber.py
@@ -1,68 +1,37 @@
-from turtle import title
+# https://github.com/korea539/ais7_demo
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import koreanize_matplotlib
-import plotly.express as px
+import numpy as np
 
-st.set_page_config(
-    page_title="Likelion AI school 자동차 연비 App",
-    page_icon="🚗",
-    layout="wide"
-)
+st.title('Uber pickups in NYC')
 
-
-st.markdown("# 자동차 연비 🚗")
-st.sidebar.markdown("# 자동차 연비 🚗")
-
-url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/mpg.csv"
-data = pd.read_csv(url)
-
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 @st.cache
-def load_data():
-    data = pd.read_csv(url)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
 data_load_state = st.text('Loading data...')
-data = load_data()
+data = load_data(10000)
 data_load_state.text("Done! (using st.cache)")
 
-st.sidebar.header('검색 기능')
-selected_year = st.sidebar.selectbox('Year',
-   list(reversed(range(data.model_year.min(), data.model_year.max())))
-   )
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
 
-if selected_year > 0 :
-   data = data[data.model_year == selected_year]
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
 
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-# Sidebar - origin
-sorted_unique_origin = sorted(data.origin.unique())
-selected_origin = st.sidebar.multiselect('origin', sorted_unique_origin, sorted_unique_origin)
-
-if len(selected_origin) > 0:
-   data = data[data.origin.isin(selected_origin)]
-
-
-st.dataframe(data)
-
-st.line_chart(data["mpg"])
-
-st.bar_chart(data["mpg"])
-
-fig, ax = plt.subplots(figsize = (10, 3))
-sns.barplot(data=data, x="origin", y="mpg").set_title("origin 별 자동차 연비")
-st.pyplot(fig)
-
-figb, axb = plt.subplots(figsize = (10, 3))
-sns.countplot(data = data, x = "origin").set_title("origin 숫자")
-st.pyplot(figb)
-
-figc, axc = plt.subplots(figsize = (10, 3))
-sns.pointplot(data = data, x = "cylinders", y = "horsepower").set_title("실린더 별 마력")
-st.pyplot(figc)
-
-pxh = px.histogram(data, x = "origin", title = "지역별 자동차 연비 데이터 수")
-st.plotly_chart(pxh)
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
